de9e7be46b74e960401c157331a84fa364dfd08f
csharpinthecsharp <trilllucien@gmail.com>
2026-07-07 17:19:25 +0200

    start opt

--- main.rs
+++ main.rs
@@ -2,10 +2,38 @@ use std::env; // standard library
 use std::process;
 use std::fs::File;
 
+struct Opt {
+    v: bool,
+    n: bool,
+    t: bool,
+}
+
+impl Opt {
+    fn setv(&mut self) {
+        self.v = true;
+    }
+    fn setn(&mut self) {
+        self.n = true;
+    }
+    fn sett(&mut self) {
+        self.t = true;
+    }
+}
+
+Impl Default for Opt {
+    fn default() -> Self {
+        Self { v: false, n: false, t: false }
+    }
+}
+
 fn ft_error(content: &str, attachement: &str) -> ! // fn never come back
 {
     eprintln!("ft_lex: {} {}", content, attachement);
     process::exit(1);
+}
+
+fn parse_opt(args: &Vec<String>)> 
+{
 }
 
 fn main()
@@ -15,6 +43,10 @@ fn main()
         println!("Using stdin");
         process::exit(0);
     }
+    
+    let mut sOpt = Opt::default();
+    // sOpt.setv();
+
     let mut _file = match File::open(&args[1]) {
         Ok (file) => file,
         Err(_) => {
