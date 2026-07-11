bf3c61ee9d5f8008c09d1b0593dd8fa808d986c9
csharpinthecsharp <trilllucien@gmail.com>
2026-07-11 14:42:54 +0200

    kf

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,11 +1,14 @@
-use crate::structs::tokens;
+use crate::structs::tokens::TOK::FirstBlock;
+use crate::structs::tokens::{self, Token};
+
 use std::iter::Peekable;
 use std::str::Chars;
 use std::fs;
 
-fn skip_line(chars: &mut Peekable<Chars>) {
+fn skip_line(chars: &mut Peekable<Chars>, line: &mut i32) {
     while let Some(&c) = chars.peek() {
         if c == '\n' {
+            *line+=1;
             break;
         }
         chars.next();
@@ -14,33 +17,52 @@ fn skip_line(chars: &mut Peekable<Chars>) {
 
 pub fn lexer_loop(path: &str)
 {
-    let mut first_part = false;
-    let mut second_part = false;
+    let mut open_block = false;
     let mut line = 1;
+    let mut column = 0;
     let content = fs::read_to_string(path).unwrap();
     let mut chars: Peekable<Chars> = content.chars().peekable();
+    let mut strbuf: String = Default::default();
+    let mut tokens: Vec<Token> = vec![];
     while let Some(c) = chars.next() {
         match c {
             '\n' => {
+                if open_block {
+                    let mut token = Token::default();
+                    token.update(FirstBlock, strbuf.clone(), line, column);
+                    tokens.push(token);
+                    strbuf.clear();
+                }
                 line+=1;
             }
             '%' => {
                 if chars.peek() == Some(&'{') {
-                    first_part = true;
+                    open_block = true;
+                    skip_line(&mut chars, &mut line);
+                }
+                if chars.peek() == Some(&'}') {
+                    open_block = false;
+                    skip_line(&mut chars, &mut line);
+                }
+                if chars.peek() == Some(&'%')  {
+                    // Exit First PART
                 }
             }
             _ => {
-                if !first_part && !second_part {
-                    if c.is_alphabetic() {
-                        eprintln!("{}:{}: incomplete name definition", path, line);
-                        skip_line(&mut chars);
-                        line+=1;
-                    }
-                    else {
-                        eprintln!("{}:{}: bad character: {}", path, line, c)
-                    }
+                if open_block {
+                    strbuf.push(c);
+                }
+                if c.is_alphabetic() {
+                    eprintln!("{}:{}: incomplete name definition", path, line);
+                    skip_line(&mut chars, &mut line);
+                }
+                else {
+                    eprintln!("{}:{}: bad character: {}", path, line, c)
                 }
             }
         }
+    }
+    if open_block {
+        eprintln!("{}:{}: premature EOF", path, line); 
     }
 }
--- src/structs/tokens.rs
+++ src/structs/tokens.rs
@@ -1,4 +1,7 @@
+use std::io::empty;
+
 pub enum TOK {
+    Default,
     FirstBlock, // %{...%}
     Name, // DIGIT
     Pattern, // [0-9] or {DIGIT}+
@@ -11,6 +14,20 @@ pub enum TOK {
 pub struct Token {
    typ: TOK,
    s: String,
-   line: usize,
-   column: usize,
+   line: i32,
+   column: i32,
+}
+
+impl Default for Token
+{
+    fn default() -> Self {
+        Self { typ: TOK::Default, s: String::new(), line: 1, column: 0 }
+    }
+}
+ 
+impl Token
+{
+    pub fn update(&mut self, ty: TOK, ss: String, l: i32, c: i32) -> Self {
+        Self { typ: ty, s: ss, line: l, column: c }
+    }
 }
